f0a78681e67eee79e3060ef260eaaab444bcd4c9
X-SaraX <saradragomir6@gmail.com>
2026-04-25 23:01:22 +0200

    Añadir ejercicios asincronía

--- Ejericios-ES6+-asincronía/Ejercicio-1.js
+++ Ejericios-ES6+-asincronía/Ejercicio-1.js
@@ -7,5 +7,21 @@ const game = {
   year: 2020,
 };
 
-const { tittle, gender, year } = game;
-console.log(game);
+const { title, gender, year } = game;
+console.log(title, gender, year);
+
+/*1.2 En base al siguiente javascript, usa destructuring para crear 3 variables llamadas 
+fruit1, fruit2 y fruit3, con los valores del array. Posteriormente imprimelo por consola.*/
+const fruits = ["Banana", "Strawberry", "Orange"];
+
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1, fruit2, fruit3);
+
+//1.3 En base al siguiente javascript, usa destructuring para crear 2 variables igualandolo a la función e imprimiendolo por consola.
+const animalFunction = () => {
+  return { name: "Bengal Tiger", race: "Tiger" };
+};
+
+const { name, race } = animalFunction;
+
+console.log(name, race);
