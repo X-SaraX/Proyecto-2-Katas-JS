c40a1df8fcd7fda4cccef2680423cc3e8bc52f9e
X-SaraX <saradragomir6@gmail.com>
2026-04-27 21:56:18 +0200

    fin del ejercicio 1 en ES6+

--- Ejericios-ES6+-asincronía/Ejercicio-1.js
+++ Ejericios-ES6+-asincronía/Ejercicio-1.js
@@ -22,6 +22,18 @@ const animalFunction = () => {
   return { name: "Bengal Tiger", race: "Tiger" };
 };
 
-const { name, race } = animalFunction;
+const { name, race } = animalFunction();
 
 console.log(name, race);
+
+//1.4 En base al siguiente javascript, usa destructuring para crear las variables name y itv con sus respectivos valores.
+// Posteriormente crea 3 variables usando igualmente el destructuring para cada uno de los años y
+// comprueba que todo esta bien imprimiendolo.
+
+const car = { name0: "Mazda 6", itv: [2015, 2011, 2020] };
+
+const { name0, itv } = car;
+const [itv1, itv2, itv3] = itv;
+
+console.log(name0, itv);
+console.log(itv1, itv2, itv3);
